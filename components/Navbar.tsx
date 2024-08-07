22e75f064b4c7211f769dad00b969de450ab9e5a
hobaaaa <denizgokbudak@gmail.com>
2024-08-07 14:31:43 +0300

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -11,14 +12,17 @@ function Navbar() {
   };
 
   return (
-    <nav className="absolute top-0 z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-md bg-slate-800">
+    <nav className="absolute top-0 z-50 w-full flex flex-wrap items-center justify-between px-2 py-1 navbar-expand-md bg-cyan-500">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-          <Link
-            href="/"
-            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-          >
-            Portfolyo-App
+          <Link href="/">
+            <Image
+              src="/images/logo.png"
+              width={250}
+              height={50}
+              alt="Site Logo"
+              className="mx-auto"
+            ></Image>
           </Link>
           <button
             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -31,14 +35,14 @@ function Navbar() {
         <div
           className={`${
             isNavbarOpen ? "" : "hidden"
-          } lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none bg-blueGray-800`}
+          } lg:flex flex-grow  lg:bg-opacity-0 lg:shadow-none bg-cyan-500 pt-3`}
           id="example-collapse-navbar"
         >
           <ul className="flex flex-col lg:flex-row list-none ml-auto">
             <li className="flex items-center">
               <Link
                 href="/projects"
-                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 md:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               >
                 <i className="fas fa-arrow-alt-circle-right"></i> Projects
               </Link>
@@ -46,16 +50,16 @@ function Navbar() {
             <li className="flex items-center">
               <Link
                 href="/about"
-                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 md:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               >
-                <i className="far fa-file-alt leading-lg mr-2 md:text-grey-200 text-grey-400"></i>{" "}
+                <i className="far fa-file-alt leading-lg mr-2 md:text-grey-200 text-grey-400"></i>
                 About Me
               </Link>
             </li>
             <li className="flex items-center">
               <Link
                 href="/contact"
-                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 md:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                className="bg-white text-slate-700 active:bg-slate-100 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:bg-slate-500 hover:text-white outline-none focus:outline-none md:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               >
                 <i className="fa-solid fa-square-phone"></i> Contact
               </Link>
