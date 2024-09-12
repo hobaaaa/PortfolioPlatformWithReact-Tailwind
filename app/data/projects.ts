004edbba7a41d61046c7ac3f30ca10b75ed89b6f
hobaaaa <denizgokbudak@gmail.com>
2024-09-12 14:42:09 +0300

    project

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -21,25 +21,25 @@ const projectsData: Project[] = [
   {
     title: "Atomm Petshop",
     description: "Atomm Petshop firması için yaptığımız websitemizdir.",
-    images: ["/images/project31.png", "/images/project32.png", "/images/project32.png", "/images/project34.png"],
+    images: ["/images/project31.png", "/images/project32.png", "/images/project33.png", "/images/project34.png"],
     slug: "project-3",
   },
   {
-    title: "Weather App",
-    description: "Weather App Next.JS ile yapılmış bir havadurumu uygulamasıdır.",
-    images: ["/images/project11.png", "/images/project11.png", "/images/project11.png"],
+    title: "Blog App",
+    description: "Blog App Next.JS ile yapılmış bir blog uygulamasıdır.",
+    images: ["/images/project41.png", "/images/project42.png", "/images/project43.png", "/images/project44.png"],
     slug: "project-4",
   },
   {
-    title: "Blog App",
-    description: "Blog App Next.JS ile yapılmış bir blog uygulamasıdır..",
-    images: ["/images/project11.png", "/images/project11.png", "/images/project11.png"],
+    title: "Weather App",
+    description: "Weather App Next.JS ile yapılmış bir hava durumu uygulamasıdır..",
+    images: ["/images/project51.png", "/images/project52.png", "/images/project53.png", "/images/project54.png"],
     slug: "project-5",
   },
   {
     title: "News App",
     description: "News App Next.JS ile yapılmış bir haberler uygulamasıdır.",
-    images: ["/images/project11.png","/images/project11.png","/images/project11.png"],
+    images: ["/images/project61.png", "/images/project62.png", "/images/project63.png", "/images/project64.png"],
     slug: "project-6",
   },
   // Dİğer Projeleri buraya ekleyeceğiz.
